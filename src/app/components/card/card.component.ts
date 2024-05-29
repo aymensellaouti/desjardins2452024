37aeb9b63c057a5d907d9356d3248709e9bbac8e
aymensellaouti <aymen.sellaouti@gmail.com>
2024-05-29 19:49:23 +0100

    fin exo card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-
+  // Les propriétés d'états
+  firstname = 'aymen';
+  name = 'sellaouti';
+  job = 'trainer';
+  path = 'rotating_card_profile3.png';
 }

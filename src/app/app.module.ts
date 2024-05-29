0393cdee4622bc448e94bd6aa45d75f12e32a5b2
aymensellaouti <aymen.sellaouti@gmail.com>
2024-05-29 20:03:49 +0100

    exemple input et exo somme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { ColorComponent } from './components/color/color.component';
 import { TwoWayComponent } from './components/two-way/two-way.component';
 import { FormsModule } from '@angular/forms';
 import { CardComponent } from './components/card/card.component';
+import { PereComponent } from './components/pere/pere.component';
+import { FilsComponent } from './components/fils/fils.component';
+import { SommeComponent } from './components/somme/somme.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,10 @@ import { CardComponent } from './components/card/card.component';
     ThirdComponent,
     ColorComponent,
     TwoWayComponent,
-    CardComponent
+    CardComponent,
+    PereComponent,
+    FilsComponent,
+    SommeComponent
   ],
   imports: [
     BrowserModule,

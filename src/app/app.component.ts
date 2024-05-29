8c4042a4d177a51242b75e65b4b244615eb4850a
aymensellaouti <aymen.sellaouti@gmail.com>
2024-05-29 16:07:55 +0100

    add Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // State
   title = 'desjardins2452024';
+  // Behaviour
+  sayHello() {
+    console.log('Hello Desjardins');
+  }
 }
